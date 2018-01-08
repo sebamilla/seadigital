ceaecf037885a5547e4ac484d8f3c86935607585
Chad Graybeal <chad@stackedit.co>
2018-01-08 16:50:27 +1300

    add contact route

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/contact', function(req, res){
+  res.render('contact');
+});
+
 module.exports = router;
